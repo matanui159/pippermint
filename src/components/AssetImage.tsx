ef0db229de5b748e8bb7680e88c4bd0a0b6deacb
matanui159 <josh@minteronline.com>
2021-09-04 10:59:21 +1000

    Add a11y description

--- src/components/AssetImage.tsx
+++ src/components/AssetImage.tsx
@@ -7,7 +7,8 @@ export interface AssetImageProps {
 }
 
 export function AssetImage({ image, priority = false }: AssetImageProps): JSX.Element {
-   const { file, title } = image.fields;
+   const { file, title, description } = image.fields;
+   const { id } = image.sys;
 
    return (
       <div className='my-8'>
@@ -18,7 +19,13 @@ export function AssetImage({ image, priority = false }: AssetImageProps): JSX.El
             width={file.details.image?.width ?? 0}
             height={file.details.image?.height ?? 0}
             priority={priority}
+            aria-describedby={description === undefined ? undefined : id}
          />
+         {description !== undefined && (
+            <div className='hidden' id={id}>
+               {description}
+            </div>
+         )}
       </div>
    );
 }
